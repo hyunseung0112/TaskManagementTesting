a539baa716ff2c6ede7dccc77bafdfc9e071b619
hyunseung0112 <hs.kim@fanmaum.com>
2026-03-20 09:57:19 +0900

    :sparkles: :: 작업 테스트

--- app/src/main/java/com/khs/myapplication/MainActivity.kt
+++ app/src/main/java/com/khs/myapplication/MainActivity.kt
@@ -42,6 +42,6 @@ fun Greeting(name: String, modifier: Modifier = Modifier) {
 @Composable
 fun GreetingPreview() {
     MyApplicationTheme {
-        Greeting("Androiㅇㅇㅇㅇ")
+        Greeting("안녕하세요")
     }
 }
